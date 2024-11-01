1f74084197569948fdbe74c09f83c6753f3638ae
Kalle Kahri <kahrikallee@gmail.com>
2024-11-01 13:26:58 +0200

    added dependecies, configured host and port as well

--- data/index.js
+++ data/index.js
@@ -1,3 +1,11 @@
+const express = require('express');
 const {XMLBuilder, XMLParser, XMLValidator} = require('fast-xml-parser');
 const mysql = require('mysql');
 const dbconfig = require('./dbconfig.json');
+
+const app = express();  
+
+const port = "4000";
+const host = "localhost"; // TODO: selvitä ip osoite ja pyöritä palvelinta sillä osoitteella localhostin sijaan
+
+app.listen(port, host, () => console.log(`${host}:${port} kuuntelee...`));
--- web/index.js
+++ web/index.js
@@ -3,3 +3,16 @@ const path = require('path');
 const bcrypt = require('bcrypt');
 const fetch = (...args) =>
     import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'public'));
+
+app.use(express.static(__dirname + '/public'));
+
+const port = "3000";
+const host = "localhost"; // TODO: selvitä ip osoite ja pyöritä palvelinta sillä osoitteella localhostin sijaan
+
+app.listen(port, host, () => console.log(`${host}:${port} kuuntelee...`));
