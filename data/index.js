0b8843d929eb29540d8c3f1ad49f02eb38b93929
Kalle Kahri <kahrikallee@gmail.com>
2024-11-01 10:03:10 +0200

    updated dependacies, changes to the file structure

---
+++ data/index.js
@@ -0,0 +1,3 @@
+const {XMLBuilder, XMLParser, XMLValidator} = require('fast-xml-parser');
+const mysql = require('mysql');
+const dbconfig = require('./dbconfig.json');
--- web/index.js
+++ web/index.js
@@ -1,7 +1,4 @@
 const express = require('express');
-const {XMLBuilder, XMLParser, XMLValidator} = require('fast-xml-parser');
-const mysql = require('mysql');
-const dbconfig = require('./dbconfig.json');
 const path = require('path');
 const bcrypt = require('bcrypt');
 const fetch = (...args) =>
