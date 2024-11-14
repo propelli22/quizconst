6e4047f89b668018830dd48088e80c491d017530
Kalle Kahri <kahrikallee@gmail.com>
2024-11-14 12:37:47 +0200

    Multiple changes, worked user register and login backend, visual changes on frontpage modals

--- web/index.js
+++ web/index.js
@@ -3,8 +3,11 @@ const path = require('path');
 const bcrypt = require('bcrypt');
 const fetch = (...args) =>
     import('node-fetch').then(({default: fetch}) => fetch(...args));
+const dotenv = require('dotenv')
 
 const app = express();
+app.use(express.urlencoded({extended: 'false'}))
+app.use(express.json())
 
 // import languages from json
 const fi_home = require('./languages/fi_home.json')
@@ -21,6 +24,8 @@ const port = "3000";
 const host = "0.0.0.0"; // run on device local ip
 
 app.get('/', async (req, res) => {
+    console.log("loaded Frontpage");
+
     const language = req.query.language;
 
     let subjectsURL = 'http://localhost:4000/getsubjects'
@@ -57,6 +62,7 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/create', (req, res) => {
+    console.log("loaded Create a game");
 	const language = req.query.language;
 
 	if (language == 'fi') {
@@ -66,6 +72,57 @@ app.get('/create', (req, res) => {
 	} else {
 		res.render('create_a_game', fi_create)
 	}
+});
+
+app.post('/register', (req, res) => {
+    console.log("used Register");
+
+    const {username, email, password} = req.body;
+    
+    const saltRounds = 10;
+
+    bcrypt.hash(password, saltRounds, async function(err, hash) {
+        const body = {
+            user: username,
+            email: email,
+            password: hash
+        }
+
+        const responseCheck = await fetch(`http://localhost:4000/checkuser?user=${username}&email=${email}`, {
+            method: 'GET'
+        });
+
+        const checkData = await responseCheck.json();
+
+        if (checkData == {"message": "OK"}) {
+            const responeCreate = await fetch('http://localhost:4000/createuser', {
+                method: 'POST',
+                body: JSON.stringify(body),
+                headers: {'Content-Type': 'application/json'}
+            });
+    
+            const data = await responeCreate.json();
+    
+            res.json(data);
+        } else {
+            res.json(checkData)
+        }
+    });
+
+});
+
+app.get('/login', async (req, res) => {
+    console.log("used Login");
+
+    const {username, password} = req.query;
+
+    const checkLogin = await fetch(`http://localhost:4000/checklogin?user=${username}&password=${password}`, {
+        method: 'GET'
+    });
+
+    const checkResult = await checkLogin.json();
+
+    res.json(checkResult);
 });
 
 app.listen(port, host, () => console.log(`Listening on ${host}:${port}...`));
