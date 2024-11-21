d08d4026cd6dfde7bb4645cd6b894ec58ddf9417
Kalle Kahri <kahrikalle@gmail.com>
2024-11-21 19:55:10 +0200

    Continued game front end

---
+++ web/public/script/game.js
@@ -0,0 +1,16 @@
+const lobbyId = 0;  // placeholder for now, until the lobby sends cookies.
+const subjectId = 2; // placeholder for now, until the lobby sends cookies.
+let questionId = 1; // placeholder for now, retrive all question ID:s of a subject on game start as an list.
+const playerId = 3; // placeholder for now, until the lobby sends cookies.
+
+
+
+// In comment tags for now, continue later when data is being fed to the game from the database
+// addEventListener("DOMContentLoaded", (event) => {
+//     const questionDiv = document.getElementById("question-container");
+//     const gameDiv = document.getElementById("main-game-container");
+//     const loadingDiv = document.getElementById("loading-container");
+//     const resultsDiv = document.getElementById("results-container");
+// 
+//     questionDiv.style.display = "block";
+// });
